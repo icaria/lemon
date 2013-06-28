e1e87daec6236b5bded21b9bb9e9edfbb76fee15
Stephen Chen <stephenchen0@gmail.com>
2013-06-27 23:15:53 -0400

    Account + Category + entity framework

---
+++ Lemon.Library/Models/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemon.Models
+{
+    public class Account
+    {
+        [Key]
+        public int AccountId { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+        public int Balance { get; set; }
+
+    }
+}
